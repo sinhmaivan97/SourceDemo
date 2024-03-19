1e1c35688ef07f7c45e498164fab3c2910d81cc2
William <sinhmaivan97@gmail.com>
2024-03-19 10:55:36 +0700

    update framework

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Source demo
+
 - Basic information about the source demo project
-  - Language: Java, Gherkin
-  - Tools: Selenium, Cucumber BDD
-  - Framework: TestNG
-  - Design Pattern: Page Object Model(POM), Singleton Pattern
-  - Report: Extent report
+    - Language: Java, Gherkin
+    - Tools: Selenium, Cucumber BDD
+    - Framework: TestNG
+    - Design Pattern: Page Object Model(POM), Singleton Pattern
+    - Report: Extent report
 
 
-- 3 small features were selected to write test scripts: login, search, change language. Access the link src/test/resources/features to see details of each scenario for each feature.
+- 3 small features were selected to write test scripts: login, search, change language. Access the link
+  src/test/resources/features to see details of each scenario for each feature.
   ![Hình minh họa](https://github.com/sinhmaivan97/repoIMG/blob/main/h%C3%ACnh%201.png)
 
 - Access the path src/test/java/cucumberHooks/Hooks to choose a browser to run test scripts.
--- src/test/java/runners/TestRunnerTC.java
+++ src/test/java/runners/TestRunnerTC.java
@@ -11,15 +11,10 @@ import java.io.File;
 @Test
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"steps", "cucumberHooks"},
-        plugin = {"cucumberHooks.CucumberListener", "pretty",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-                "html:target/cucumber-reports/cucumber-reports.html",
-                "json:target/cucumber-reports/cucumber-reports.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
-        tags = "@Research"
-//        tags = "@ChangeLanguage"
-//        tags = "@Signin"
+        tags = "@Research or @ChangeLanguage"
+//        tags = "not @Signin"
 )
 public class TestRunnerTC extends AbstractTestNGCucumberTests {
     @Override
@@ -32,25 +27,17 @@ public class TestRunnerTC extends AbstractTestNGCucumberTests {
     public void deleteScreenshotsFiles() {
         try {
             System.out.println("================ BEFORE SUITE ================");
-            String pathFolderallure = "allure-results";
+
             String pathFolerScreenshot = "extentReport/screenshots";
-
-            File fileAllure = new File(pathFolderallure);
             File fileScreenshot = new File(pathFolerScreenshot);
-
-            File[] listOfFilesAllure = fileAllure.listFiles();
             File[] listOfFilesScreenshot = fileScreenshot.listFiles();
-            System.out.println("......................" + pathFolderallure);
-            for (int i = 0; i < listOfFilesAllure.length; i++) {
-                if (listOfFilesAllure[i].isFile()) {
-                    new File(listOfFilesAllure[i].toString()).delete();
-                }
-            }
+
             for (int i = 0; i < listOfFilesScreenshot.length; i++) {
                 if (listOfFilesScreenshot[i].isFile()) {
                     new File(listOfFilesScreenshot[i].toString()).delete();
                 }
             }
+
             System.out.println("================ DELETE ================");
         } catch (Exception e) {
             System.out.println(e.getMessage());
